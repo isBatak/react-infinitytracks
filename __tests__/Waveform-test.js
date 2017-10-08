7597e578fde1c36221fe8d5ed09c4864e6ff2c41
Ivica Batinic <ivica.batinic@infinum.hr>
2017-10-08 21:28:12 +0200

    Fix tests

--- __tests__/Waveform-test.js
+++ __tests__/Waveform-test.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import { Waveform } from 'react-infinitytracks';
 import renderer from 'react-test-renderer';
+import 'web-audio-test-api';
 
-const audioCtx = new AudioContext();
-const myArrayBuffer = audioCtx.createBuffer(2, audioCtx.sampleRate * 3, audioCtx.sampleRate);
+const audioContext = new AudioContext();
+const buffer = audioContext.createBuffer(1, 2024, audioContext.sampleRate);
+const data = buffer.getChannelData(0);
 
-test('Show wavefrom', () => {
-  const component = renderer.create(
-    <Waveform buffer={myArrayBuffer} />
-  );
-  let tree = component.toJSON();
+let counter = 0;
+const increase = (Math.PI * 2) / 100;
+
+for (let i = 0; i < 2024; i++) {
+  data[i] = (Math.sin(counter) / 2) + 0.5;
+  counter += increase;
+}
+
+test('Show waveform', () => {
+  const component = renderer.create(<Waveform buffer={buffer} width={720} color="cadetblue" />);
+  const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 });
