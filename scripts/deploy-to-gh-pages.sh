20fc24befb0076ba1755385c19cb32049beb07cd
Ivica Batinic <ivica.batinic@infinum.hr>
2017-10-15 21:45:04 +0200

    Update deploy sh

--- scripts/deploy-to-gh-pages.sh
+++ scripts/deploy-to-gh-pages.sh
@@ -7,7 +7,7 @@ rm -rf storybook-static
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 
-# build (CHANGE THIS)
+# build
 yarn build-storybook
 
 # deploy
@@ -15,4 +15,4 @@ cd storybook-static
 git init
 git add .
 git commit -m "Deploy to Github Pages"
-git push --force --quiet "https://${GITHUB_TOKEN}@$github.com/${GITHUB_REPO}.git" master:gh-pages > /dev/null 2>&1
+git push --force --quiet "https://${GITHUB_TOKEN}@$github.com/${GITHUB_REPO}.git" master:gh-pages
