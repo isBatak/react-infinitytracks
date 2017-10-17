d24b2c61c7fdfa19416a118359dd3bd1f20a3cc2
Ivica Batinic <ivica.batinic@infinum.hr>
2017-10-17 22:42:14 +0200

    WIP on loading wav file

--- stories/index.js
+++ stories/index.js
@@ -6,14 +6,14 @@ import { storiesOf } from '@storybook/react';
 
 import { Waveform } from 'react-infinitytracks';
 
-import WaveformComponent from './waveform/WaveformComponent';
-
 import Utils from './utils';
 
 const context = new (window.AudioContext || window.webkitAudioContext)();
 const sineWaveBuffer = Utils.sineWave(context);
+const wavBuffer = Utils.loadWavBuffer(context, 'https://waveform.000webhostapp.com/DS_BassA140D-01.wav');
 
 storiesOf('Waveform', module)
-  .add('canvas and .wav', () => <WaveformComponent audioContext={context} />)
+  .add('canvas and .wav', () => <Waveform datum={wavBuffer.getChannelData(0)} width={720} color="cadetblue" />)
   .add('canvas and sineWave', () => <Waveform datum={sineWaveBuffer.getChannelData(0)} width={720} color="cadetblue" />)
-  .add('svg and sineWave', () => <Waveform datum={sineWaveBuffer.getChannelData(0)} width={720} color="blue" renderingMode={Waveform.renderMod.CANVAS} />);
+  .add('svg and .wav', () => <Waveform datum={wavBuffer.getChannelData(0)} width={720} color="cadetblue" renderingMode={Waveform.renderMod.SVG} />)
+  .add('svg and sineWave', () => <Waveform datum={sineWaveBuffer.getChannelData(0)} width={720} color="cadetblue" renderingMode={Waveform.renderMod.SVG} />);
