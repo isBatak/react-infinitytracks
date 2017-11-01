a74d549a499fb99a42f72901b138a3054ebf8a7f
Ivica Batinic <ivica.batinic@infinum.hr>
2017-11-01 22:42:05 +0100

    Add length prop to sineWave util

--- stories/utils.js
+++ stories/utils.js
@@ -1,12 +1,12 @@
 const utils = {
-  sineWave(audioContext) {
-    const buffer = audioContext.createBuffer(1, 2024, audioContext.sampleRate);
+  sineWave(audioContext, length = 2024) {
+    const buffer = audioContext.createBuffer(1, length, audioContext.sampleRate);
     const data = buffer.getChannelData(0);
 
     let counter = 0;
     const increase = (Math.PI * 2) / 100;
 
-    for (let i = 0; i < 2024; i++) {
+    for (let i = 0; i < length; i++) {
       data[i] = (Math.sin(counter) / 1);
       counter += increase;
     }
