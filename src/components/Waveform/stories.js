6710f2a235fb17884d9a58f99adab938e1b24c10
Ivica Batinic <ivica.batinic@infinum.hr>
2017-12-09 20:48:27 +0100

    Moved storyes in component folder

--- src/components/Waveform/stories.js
+++ src/components/Waveform/stories.js
@@ -1,27 +1,10 @@
 import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { Waveform, AudioRenderProps } from 'react-infinitytracks';
+import { getContext, sineWave } from '../../../stories/utils';
 
-import { storiesOf, addDecorator } from '@storybook/react';
-// import { action } from '@storybook/addon-actions';
-// import { linkTo } from '@storybook/addon-links';
-
-import { Waveform, AudioProvider, AudioRenderProps } from 'react-infinitytracks';
-
-import Utils from './utils';
-
-const context = new (window.AudioContext || window.webkitAudioContext)();
-
-const sineWaveBuffer = Utils.sineWave(context);
-
-const AudioProviderDecorator = storyFn => (
-  <AudioProvider
-    url="https://waveform.000webhostapp.com/DS_BassA140D-01.wav"
-    audioContext={context}
-  >
-    { storyFn() }
-  </AudioProvider>
-);
-
-addDecorator(AudioProviderDecorator);
+const context = getContext();
+const sineWaveBuffer = sineWave(context);
 
 storiesOf('Waveform', module)
   .add('canvas and .wav', () => (
@@ -40,3 +23,4 @@ storiesOf('Waveform', module)
     />
   ))
   .add('svg and sineWave', () => <Waveform buffer={sineWaveBuffer.getChannelData(0)} width={720} color="cadetblue" renderingMode={Waveform.renderMod.SVG} />);
+
--- stories/utils.js
+++ stories/utils.js
@@ -1,18 +1,24 @@
-const utils = {
-  sineWave(audioContext) {
-    const buffer = audioContext.createBuffer(1, 2024, audioContext.sampleRate);
-    const data = buffer.getChannelData(0);
+let context;
 
-    let counter = 0;
-    const increase = (Math.PI * 2) / 100;
+export function getContext() {
+  if (typeof context === 'undefined') {
+    context = new (window.AudioContext || window.webkitAudioContext)();
+  }
 
-    for (let i = 0; i < 2024; i++) {
-      data[i] = (Math.sin(counter) / 1);
-      counter += increase;
-    }
+  return context;
+}
 
-    return buffer;
-  },
-};
+export function sineWave(audioContext) {
+  const buffer = audioContext.createBuffer(1, 2024, audioContext.sampleRate);
+  const data = buffer.getChannelData(0);
 
-export default utils;
+  let counter = 0;
+  const increase = (Math.PI * 2) / 100;
+
+  for (let i = 0; i < 2024; i++) {
+    data[i] = (Math.sin(counter) / 1);
+    counter += increase;
+  }
+
+  return buffer;
+}
