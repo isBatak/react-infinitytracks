edc7133b585cade859bb58e562e1e12440ea831b
Ivica Batinic <ivica.batinic@infinum.hr>
2017-12-10 21:33:33 +0100

    Remove commented code from axis story

--- src/components/Axis/stories.js
+++ src/components/Axis/stories.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { Axis } from 'react-infinitytracks';
-// import { getContext, sineWave } from '../../../stories/utils';
-
-// const context = getContext();
-// const sineWaveBuffer = sineWave(context);
 
 storiesOf('Axis', module)
   .add('axis', () => (
